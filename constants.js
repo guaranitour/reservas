d73a634884d4b9fbd50aa6647c735d326a6acda3
guaranitour <merelesrichard@gmail.com>
2026-01-24 09:44:40 -0300

    Delete constants.js

--- constants.js
+++
@@ -1,7 +0,0 @@
-// lib/constants.js
-export const BASE_URL = 'https://script.google.com/macros/s/AKfycbxp22XHzfE8JLdVkePwa6xq4ixw3xhCoxioqplh_BkzMCwLCiYAql52xwmYRnpcN475/exec';
-export const API_KEY = null; // reservado
-export const GOOGLE_CLIENT_ID = '273442733710-1eqf1erm1vl9vsad2lb4krennldt1jhf.apps.googleusercontent.com';
-export const CACHE_PREFIX = 'seatapp_';
-export const STAFF_KEY = 'app_staff_mode';
-export const ROLES = { ADMIN: 'admin', VIEWER: 'viewer' };
